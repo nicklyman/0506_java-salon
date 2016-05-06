626b0e460ad79ff023e4945d3e654bc8313e88cf
Nick Lyman <nick.lyman@gmail.com>
2016-05-06 10:51:29 -0700

    Add DatabaseRule.java file to clear out the test database after each test and DRY my three .java test files.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,11 @@
+import org.sql2o.*; // for DB support
+import org.junit.*; // for @Before and @After
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
@@ -16,6 +18,9 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void rootTest() {
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,24 +1,13 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.junit.rules.ExternalResource;
 import java.util.Arrays;
 
 public class ClientTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-  }
-
-  @After
-  public void tearDown() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteStylistsQuery = "DELETE FROM stylists *;";
-      String deleteClientsQuery = "DELETE FROM clients *;";
-      con.createQuery(deleteStylistsQuery).executeUpdate();
-      con.createQuery(deleteClientsQuery).executeUpdate();
-    }
-  }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void Client_instantiatesCorrectly_true() {
